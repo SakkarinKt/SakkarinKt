53b58ec3eca8891c59a85075be4d453957c27bde
SakkarinKt <sakkarink12@gmail.com>
2025-12-02 22:03:40 +0800

    fix(ui) : fix astro base path for Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://SakkarinKt.github.io',
+	base: '/SakkarinKt',
 	integrations: [mdx(), sitemap()],
 });
